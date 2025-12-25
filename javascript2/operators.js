69ba11c0b43be5c4bc325dcc0df91ba48abc4718
alessio80funky <107595438+alessio80funky@users.noreply.github.com>
2025-12-25 12:18:11 +0900

    12/25 12:18

---
+++ javascript2/operators.js
@@ -0,0 +1,32 @@
+//演算子
+
+/** 演算子のグループ　（ベース）
+ 
+ 算術：+ - * / % ** ++ --
+ インクリメント・デクリメント：　++ --
+
+ 代入：= -= += *= /= %= **=
+
+ 比較(関係)：　== !=  === !== < > >= <=
+ 
+ 論理：　&& (and) || (or)  ! (not)
+ 
+ */
+
+ // x = a + b
+
+ //xとaとbはオペランドと言います。
+ // = と + はオペレーターと言います（演算子）
+
+//その他 typeof
+
+  let str = 10;
+  console.log(typeof(str));
+
+  let person = {
+    name: "mario",
+    age:32,
+    job: "engineer"
+};
+
+console.log(typeof(person.age))
