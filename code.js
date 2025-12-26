29f2af0bff86b1cd11f5bfc692fadc2d20600983
alessio80funky <107595438+alessio80funky@users.noreply.github.com>
2025-12-26 14:29:14 +0900

    12/26 14:28

---
+++ code.js
@@ -0,0 +1,11 @@
+//クォートに関するルール
+
+const a = 10;
+const b = 20;
+
+console.log(`1 + 2 + 3 + ${a + b}`);
+console.log("1 + 2 + 3 + ${a + b}");
+console.log(`1 + 2 + 3 + "mario"`);
+
+console.log("'mario'");
+console.log('"mario"');
