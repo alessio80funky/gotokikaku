c308f718fc8409cdbdf48812b9e8f6577771af0b
alessio80funky <107595438+alessio80funky@users.noreply.github.com>
2026-01-26 10:48:54 +0900

    1/26 10:48

--- javascript6/if.js
+++ javascript6/if.js
@@ -28,14 +28,71 @@ if(x >= 10){
 //else if 
 //複数の条件を取り扱う時に使用する命令です。
 
-let score = 50;
+let score = 91;
 
 if(score >= 90){
     console.log('A');
-} else if (score >= 70){
+}else if (score >= 70){
     console.log('B');
 }else if (score >= 60){
     console.log('C');
 }else{
     console.log('D');
 };
+
+//if ネスト
+
+let age = 17;
+isMember = false;
+
+if(age >= 18){
+    if (isMember){ 
+        console.log("会員です")
+    }else{
+        console.log("会員じゃないです")
+    }
+}else{
+    console.log("未成年です。会員登録できません。")
+}
+
+//注意
+
+//波括弧を省略しない！
+
+//NG
+
+if(x > 0)
+console.log('ok')
+
+//OK
+
+if (x > 0){
+    console.log('ok')
+}
+
+//条件式に代入演算子「＝」を使わない！
+
+//NG
+
+if (x = 5){
+    console.log('ok')
+}//これを書くとバグが発生する
+
+//OK
+
+if (x === 5){
+    console.log('ok')
+}//正しい演算子
+
+
+//論理演算子を使うケース
+
+
+let age2 = 18;
+let isMember2 = false;
+
+if(age2 >= 18 || isMember2 === true){
+    console.log("会員です。")
+}else {
+    console.log("会員じゃないです")
+}
