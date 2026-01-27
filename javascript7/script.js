c9f6335e822d0a22c7eb57472382a7765995b0c2
alessio80funky <107595438+alessio80funky@users.noreply.github.com>
2026-01-27 12:20:57 +0900

    1/27 12:19

---
+++ javascript7/script.js
@@ -0,0 +1,8 @@
+//セクション❶
+
+const items = ["りんご","バナナ","苺"];
+
+const showBtn = document.getElementById("showItems");
+const clearBtn = document.getElementById("clearItems");
+const itemList = document.getElementById("itemList");
+
