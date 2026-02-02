9a189a662df1aef7c61d2556e7487a9f4b9d5d07
alessio80funky <107595438+alessio80funky@users.noreply.github.com>
2026-02-02 11:12:17 +0900

    2/2 11:12

---
+++ javascript9/code.js
@@ -0,0 +1,24 @@
+//stringメソッド
+
+const lang = "javascript";
+console.log(lang.toUpperCase());
+
+const lang2 = "JAVASCRIPT";
+console.log(lang2.toLowerCase());
+
+const lang3 = "javascript";
+console.log(lang3.indexOf("s"));
+
+const lang4 = "javascript";
+console.log(lang4.includes("a"));
+
+const lang5 = "javascript";
+console.log(lang5.slice(0, 2));
+
+const lang6 = "javascript";
+console.log(lang6.replace("java", "type"))
+
+//number メソッド　＋　mathメソッド
+
+const num = 3.14155590000000;
+console.log(num.toFixed(2));
