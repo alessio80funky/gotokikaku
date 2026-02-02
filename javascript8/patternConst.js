d3dd69a14ac3e81a90be226c89573a99aae11c4f
alessio80funky <107595438+alessio80funky@users.noreply.github.com>
2026-02-02 10:39:57 +0900

    2/2 10:39

--- javascript8/patternConst.js
+++ javascript8/patternConst.js
@@ -1,6 +1,6 @@
 //テンプレートコンストラクター❷
 //new 演算子を書くとプリミティブのデータ型がobjectとして認識されます。
-const string =  String(14);
+const string = String(14);
 
 console.log(typeof string);
 
@@ -20,6 +20,8 @@ const obj = new Object();
 
 obj.name = "kaori";
 obj.age = 29;
+obj.name2 = "tsubasa"
+obj.age2 = 21;
 
 console.log(obj);
 
