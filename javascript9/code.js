016ceebe3549818a1dfcb3eb2ffe149d2fef48dd
alessio80funky <107595438+alessio80funky@users.noreply.github.com>
2026-02-02 12:03:28 +0900

    2/2 12:03

--- javascript9/code.js
+++ javascript9/code.js
@@ -20,5 +20,30 @@ console.log(lang6.replace("java", "type"))
 
 //number メソッド　＋　mathメソッド
 
+//number メソッド
+
 const num = 3.14155590000000;
 console.log(num.toFixed(2));
+
+const num2 = 3.14155590000000;
+console.log( typeof num2.toString());
+
+function Gotokikaku( data , numeric){
+    this.data = data;
+    this.numeric = numeric;
+}
+
+const  num3  = new Gotokikaku(14,20);
+console.log(num3);
+console.log(num3.valueOf());
+
+const n = new Number(10);//一つの指定しかできない
+console.log(typeof n);
+console.log(n.valueOf());
+
+//mathメソッド
+
+
+
+
+
