7c2fedcb03bfc69f880a60f4aa92a435f991482a
alessio80funky <107595438+alessio80funky@users.noreply.github.com>
2026-02-03 10:49:01 +0900

    2/3 10:48

---
+++ javascript10/code.js
@@ -0,0 +1,19 @@
+//配列メソッド
+
+const array = ["apple", "banana"];
+
+array.push("cherry");
+console.log(array);
+
+array.pop();
+console.log(array);
+
+array.shift();
+console.log(array);
+
+array.unshift("watermelon");
+console.log(array);
+           //0 1 2 3 4 5 6 7 8
+const arr = [1,2,3,4,5,6,7,8,9];
+
+console.log(arr.slice(1,6));
