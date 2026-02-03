163e0c84640d51745c555bcac9628f66e76d4c1d
alessio80funky <107595438+alessio80funky@users.noreply.github.com>
2026-02-03 11:43:46 +0900

    2/3 11:43

--- javascript10/code.js
+++ javascript10/code.js
@@ -29,3 +29,27 @@ console.log(arr2.join("/"));
 console.log(arr2.includes("a"));
 
 console.log(arr2.indexOf("a"));
+
+const arr3 = [1000, 2000, 3000];
+
+const tax = arr3.map(a => Number((a * 1.1).toFixed(0)));
+
+console.log(tax);
+
+const ar = [1000, 2000, 3000];
+
+const TAX = [];
+
+ar.forEach(a => {
+   const tax2 = Number((a * 1.1).toFixed(0));
+   TAX.push(tax2);
+})
+
+console.log(TAX);
+
+const arr4 = [0,1,2,3,4,5,6,7,8,9];
+
+const number = arr4.filter(b => b % 3 === 0);
+
+console.log(number);
+
