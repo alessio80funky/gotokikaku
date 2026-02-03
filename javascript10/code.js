ea39dccb6d05c10a0e895a6f37c1e27698a8c860
alessio80funky <107595438+alessio80funky@users.noreply.github.com>
2026-02-03 10:57:43 +0900

    2/3 10:57

--- javascript10/code.js
+++ javascript10/code.js
@@ -17,3 +17,15 @@ console.log(array);
 const arr = [1,2,3,4,5,6,7,8,9];
 
 console.log(arr.slice(1,6));
+
+arr.splice(0,3,"X","y","C","z");
+console.log(arr);
+
+
+const arr2 = ["a", "b", "c"];
+
+console.log(arr2.join("/"));
+
+console.log(arr2.includes("a"));
+
+console.log(arr2.indexOf("a"));
