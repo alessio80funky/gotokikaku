f5063b34ed1941a7fe60ab996dd81080aa761ce4
alessio80funky <107595438+alessio80funky@users.noreply.github.com>
2026-02-04 12:43:38 +0900

    2/4 12:43

--- javascript11/app/script.js
+++ javascript11/app/script.js
@@ -14,3 +14,28 @@ const typeSelect =document.getElementById("type");
 
 
 const transactionList = document.getElementById("transactionList");
+
+
+//初期化ロジック
+
+ init();
+
+ function init(){
+    form.addEventListener("submit")
+ }
+
+//保存のロジック
+
+function saveTransactions(){
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(transactions));
+}
+
+  //ローカルストレージからの読み込みロジック
+
+  function loadTransactions(){
+    const stored = localStorage.getItem(STORAGE_KEY);
+    if (stored){
+        transactions = JSON.parse(stored)
+    }
+  }
+
